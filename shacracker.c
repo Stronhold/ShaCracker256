66e0ef81cc98de9b003666ee0f850eea8e92ff59
osboxes.org <sergioausin1993@gmail.com>
2016-05-01 11:49:43 +0100

    fixed getKey, removed warnings

--- shacracker.c
+++ shacracker.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define ALPHABET "12345"
 #define MIN 2
 #define MAX 4
@@ -7,7 +8,7 @@
 //obtenemos la potencia para un numero (funciona!)
 int getPow(int size, int pos){
 	int i;
-	auto result = 1;
+	int result = 1;
 	printf("Resultado para posicion: %i \r\n", pos);
 	for(i = 1; i <= pos;i++){
 		result *= size;
@@ -19,8 +20,8 @@ int getPow(int size, int pos){
 
 //obtenemos el keyspace (works!)
 int getKeySpace(int min, int max, int size){
-	auto i;
-	auto result = 0;
+	int i;
+	int result = 0;
 	//printf("Min recibido: %u", )
 	for(i = min; i <= max; i++){
 		result += getPow(size, i);
@@ -28,22 +29,23 @@ int getKeySpace(int min, int max, int size){
 	return result;
 }
 
-void getKey(int indice, int min, int max, char* alph){
-	auto size = min;
-	auto indexs = indice;
-	auto longitudAlfabeto = strlen(alph);
+char* getKey(int indice, int min, int max, char* alph){
+	int size = min;
+	int indexs = indice;
+	int longitudAlfabeto = strlen(alph);
 	//caracteres necesarios para representarlo
 	while(getPow(longitudAlfabeto, size)<indexs){
 		indexs -= getPow(longitudAlfabeto, size);
 		size++;
 	}
 	printf("Tamaño del array: %u \r\n", size);
-	char caracters [size+1];
-	auto i = 0;
+	
+	char *caracters = malloc(size+1);
+	int i = 0;
 
 	//inicializacion del array
 	for(i = 0; i < size; i++){
-		caracters[i] = '0';
+		caracters[i] = alph[0];
 	}
 	caracters[size] = '\0';
 	printf("%s \n", caracters);
@@ -55,18 +57,19 @@ void getKey(int indice, int min, int max, char* alph){
 		i++;
 		indexs /= longitudAlfabeto;
 	}
-	printf("%s \n", caracters);
+	return caracters;
 
 }
 
 //haciendo pruebas aun con el getkey
 int main(int argc, char *argv[]){
-	auto size = strlen(ALPHABET);
+	int size = strlen(ALPHABET);
 	printf("Size: %u \r\n", size);
-	auto keyspace = getKeySpace(MIN, MAX, size);
-	printf("KeySpace %u", keyspace);
-	getKey(1, MIN,MAX, ALPHABET);
+	int keyspace = getKeySpace(MIN, MAX, size);
+	printf("KeySpace %u \r\n", keyspace);
+	char* key = getKey(0, MIN,MAX, ALPHABET);
 	//char* clave = getKey(0,0,0,"");
-	//printf("%s", clave);
+	printf("Clave: %s \r\n", key);
+	free(key);
 	return 0;
 }
